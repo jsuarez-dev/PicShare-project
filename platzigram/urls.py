07ad4e09fcc0eb430c91f382732fb19a936e6882
johan suarez <jhoanseb1992@gmail.com>
2019-11-04 20:23:42 +1100

    fourth class create the application "post" and an example

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from platzigram import views
+from platzigram import views as local_views
+from posts import views as post_views
 
 urlpatterns = [
-    path('hello/', views.hello_world),
-    path('sorted/', views.sorted_numbers),
-    path('hi/<str:name>/<int:age>/', views.say_hi)
+    path('hello/', local_views.hello_world),
+    path('sorted/', local_views.sorted_numbers),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+    path('posts/',post_views.list_views),
 ]
---
+++ posts/views.py
@@ -0,0 +1,39 @@
+"""Post Views"""
+# Django
+from django.shortcuts import render
+from django.http import HttpResponse
+# Utilities
+from datetime import datetime
+
+
+posts =[
+    {
+        'name': 'Mont Blac',
+        'user': 'Yesica Cortez',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1036',
+    },
+    {
+        'name': 'Via Láctea',
+        'user': 'C. Vander',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=903',
+    },
+    {
+        'name': 'Nuevo auditorio',
+        'user': 'Thespianartist',
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'picture': 'https://picsum.photos/200/200/?image=1076',
+    }
+]
+
+
+def list_views(request):
+    content = []
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}"/></figure>
+        """.format(**post))
+    return HttpResponse(content)
