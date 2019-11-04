69763526e4c465ab71267976b2128c2565421c9d
johan suarez <jhoanseb1992@gmail.com>
2019-11-04 19:54:33 +1100

    third class url groups values

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -19,5 +19,6 @@ from platzigram import views
 
 urlpatterns = [
     path('hello/', views.hello_world),
-    path('hi/', views.hi),
+    path('sorted/', views.sorted_numbers),
+    path('hi/<str:name>/<int:age>/', views.say_hi)
 ]
