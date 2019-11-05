e21c77d4d6dc6d3f4ff2cbb02bd5735a55b0d65e
johan suarez <jhoanseb1992@gmail.com>
2019-11-05 11:48:45 +1100

    Sixth class we study "migrate" and "makemigration" to update and modify the sqlite data base

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Platzigram-project
 This repository has the result project of a Django course by Platzi
+
+
+### class 1:
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
+"""Post models."""
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """User model"""
+
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField()
+
+    birthdate = models.DateField(blank=True,null=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
